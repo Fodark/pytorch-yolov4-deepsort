c464b24ce0e0022b42de3abcef78b417f328884a
Nicola Dall'Asen <fodark@protonmail.com>
2021-03-23 15:49:30 +0100

    compare two detectors

--- detector/__init__.py
+++ detector/__init__.py
@@ -5,8 +5,8 @@ from .PyTorch_YOLOv4.utils.inference_model import YoloMish
 
 __all__ = ['build_detector']
 
-def build_detector(finetuned=False, use_cuda=True):
+def build_detector(finetuned=False, use_cuda=True, cfg="models/yolov4l-mish.yaml", weights="weights/yolov4l-mish.pt"):
     if finetuned:
-        return YoloMish()
+        return YoloMish(cfg=cfg, weights=weights)
     else:
         return YOLO(use_cuda)
